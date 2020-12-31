77e5c3288f95c08db49a6b3eb55ce41ec666685a
Samintha Kaveesh <skaveesh@gmail.com>
2020-12-31 13:30:15 +0530

    initial project commit

---
+++ src/main/java/com/skaveesh/res4jds/Resilience4jDownstreamApplication.java
@@ -0,0 +1,13 @@
+package com.skaveesh.res4jds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Resilience4jDownstreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Resilience4jDownstreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/skaveesh/res4jds/Resilience4jDownstreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skaveesh.res4jds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Resilience4jDownstreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
